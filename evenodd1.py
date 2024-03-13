01f0636a6dec634e847b0b72001d45c6752b28d0
Jayachitraa <jaijai28797@gmail.com>
2024-03-13 10:07:35 +0530

    modify evenodd

--- evenodd1.py
+++ evenodd1.py
@@ -2,20 +2,20 @@ number = int(input("Enter the Number:"))
 if number <= 0:
     print("Enter the vadild number")
 elif number % 2 == 0:
-    print("The given number is even!!!")
+    print("The given number {0} is even!!!".format(number))
 else:
-    print("The given number is odd number!!!")
+    print("The given number {a} is odd number!!!".format(a= number))
 
  
-def evenodd(number):
-    for i in number:
-        if i <= 0:
-            print("Enter the vadild number")
-        elif i % 2 == 0:
-            print("The given number is even!!!")
-        else:
-            print("The given number is odd number")
+# def evenodd(number):
+#     for i in number:
+#         if i <= 0:
+#             print("Enter the vadild number")
+#         elif i % 2 == 0:
+#             print("The given number is even!!!")
+#         else:
+#             print("The given number is odd number")
 
-x = [1,2,3,4,5,6,7,7,8,9,0]
-evenodd(x)
+# x = [1,2,3,4,5,6,7,7,8,9,0]
+# evenodd(x)
 
